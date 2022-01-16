30f1f7adc5ad8d1195fa94e273c787a0f930c3aa
Aleshkk <96646891+Aleshkk@users.noreply.github.com>
2022-01-16 23:55:06 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,82 @@
+let comments = [];
+loadComments();
+
+document.getElementById('comment-add').onclick = function(){
+    let commentName = document.getElementById('comment-name');
+    let commentBody = document.getElementById('comment-body');
+
+    let comment = {
+        name : commentName.value,
+        body : commentBody.value,
+        time : Math.floor(Date.now() / 1000)
+    }
+
+    commentName.value = '';
+    commentBody.value = '';
+
+    comments.push(comment);
+    saveComments();
+    showComments();
+}
+
+function saveComments(){
+    localStorage.setItem('comments', JSON.stringify(comments));
+}
+
+function loadComments(){
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
+
+function showComments (){
+    let commentField = document.getElementById('comment-field');
+    let out = '';
+    comments.forEach(function(item){
+        out += `<p class="text-right small"><em>${timeConverter(item.time)}</em></p>`;
+        out += `<p class="alert alert-primary" role="alert">${item.name}</p>`;
+        out += `<p class="alert alert-success" role="alert">${item.body}</p>`;
+    });
+    commentField.innerHTML = out;
+}
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
+  var slideIndex=1;
+  showSlides(slideIndex);
+  function plusSlides(n){
+    showSlides(slideIndex += n);
+
+  }
+  function currentSlide(n) {
+      showSlides(slideIndex=n);
+  }
+  function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n>slides.length){
+        slideIndex = 1;
+    }
+    if (n<1){
+        slideIndex=slides.length
+    }
+    for (i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+    for (i=0;i<dots.length;i++){
+        dots[i].className=dots[i].className.replace("active","");
+
+  }
+  slides[slideIndex-1].style.display="block";
+  dots[slideIndex-1].className +=" active";
+}
